8187dd407b8ec5cd1779f4a957a4a2176aa62e91
mt182001 <di211506@fhstp.ac.at>
2022-11-02 17:43:58 +0100

    test

--- dashboard.py
+++ dashboard.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('48 Minuten bis zum Identitätsdiebstahl')
+st.title('47 Minuten bis zum Identitätsdiebstahl')
 st.header('Identitäten sind heute mit der digitalen Welt verbunden oder hängen sogar schon vollständig von ihr ab. Unsere Passwörter schützen sie vor unerwünschtem Zugriff, aber wie sicher sind sie? Über 30% der Accounts sind massiv gefährdet.')
 st.subheader('Andreas Braun, René Langschwert, Florian Voglauer')
 
