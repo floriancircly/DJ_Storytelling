cbc414db27f36e26ff3cf591479a815029879db2
mt182001 <di211506@fhstp.ac.at>
2022-11-02 17:57:07 +0100

    balloons

--- dashboard.py
+++ dashboard.py
@@ -12,7 +12,7 @@ text = (
     "und Wieder \n"
     "und Wieder \n"
 )
-st.audio("https://www.youtube.com/watch?v=nnUdbEtzCBo")
+st.balloons()
 st.image("https://c.tenor.com/p0gHiSC-xecAAAAi/rainbow-dance-pepe-jam.gif")
 st.video("https://www.youtube.com/watch?v=pxw-5qfJ1dk")
 
