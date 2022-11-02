ae292aaf156c5a995168c6b79f741ab1c089fa34
mt182001 <di211506@fhstp.ac.at>
2022-11-02 17:46:28 +0100

    test2

--- dashboard.py
+++ dashboard.py
@@ -2,13 +2,14 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('47 Minuten bis zum Identitätsdiebstahl')
+st.title('48 Minuten bis zum Identitätsdiebstahl')
 st.header('Identitäten sind heute mit der digitalen Welt verbunden oder hängen sogar schon vollständig von ihr ab. Unsere Passwörter schützen sie vor unerwünschtem Zugriff, aber wie sicher sind sie? Über 30% der Accounts sind massiv gefährdet.')
 st.subheader('Andreas Braun, René Langschwert, Florian Voglauer')
 
 text = (
     "Das is eion Test \n"
     "Und wieder \n"
+    "und Wieder \n"
     "und Wieder \n"
 )
 
