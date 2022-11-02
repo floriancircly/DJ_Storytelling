0409c8038e4fbc8eda6891a5bc7243f35f4a71aa
Florian Voglauer <florian@circly.at>
2022-11-02 17:42:41 +0100

    added test content

--- dashboard.py
+++ dashboard.py
@@ -2,4 +2,15 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-st.title('Uber pickups in NYC')
+st.title('48 Minuten bis zum Identitätsdiebstahl')
+st.header('Identitäten sind heute mit der digitalen Welt verbunden oder hängen sogar schon vollständig von ihr ab. Unsere Passwörter schützen sie vor unerwünschtem Zugriff, aber wie sicher sind sie? Über 30% der Accounts sind massiv gefährdet.')
+st.subheader('Andreas Braun, René Langschwert, Florian Voglauer')
+
+text = (
+    "Das is eion Test \n"
+    "Und wieder \n"
+    "und Wieder \n"
+)
+
+
+st.text(text)
