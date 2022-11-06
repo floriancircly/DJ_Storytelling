3f279d17c4c644bc516eaa1239ab6f3dbaafc01d
andibraun00 <di211503@fhstp.ac.at>
2022-11-06 14:32:58 +0100

    Fix Title

--- dashboard.py
+++ dashboard.py
@@ -278,7 +278,8 @@ z_notation =    [
 
 fig = ff.create_annotated_heatmap(z, x=x, y=y, annotation_text=z_notation,colorscale='RdBu') # YlGnBu
 fig.update_traces(hoverinfo='skip')
-fig.update_layout(title="Vergleich der Dauer, um ein Passwort mit unterschiedlicher länge zu hacken </br>(Worst Case Szenario)",width=1500)
+fig.update_layout(title="""Vergleich der Dauer, um ein Passwort mit unterschiedlicher länge zu hacken
+                 (Worst Case Szenario)""",width=1500)
 fig.add_annotation(dict(font=dict(color='black',size=10),
                                         x=0,
                                         y=-0.30,
